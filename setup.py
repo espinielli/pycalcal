0b6a0e626b90f7b19165d1ba560567f482bdaa8f
rn123 <ray.nowell@gmail.com>
2020-01-11 17:29:41 -1000

    convert project to a package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pycalcal", # Replace with your own username
+    version="0.0.1",
+    description="Minimal package for using pycalcal.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/rn123/pycalcal",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
